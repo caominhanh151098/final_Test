6d5866b7c5895d88b86313df53fcf9a607920754
caominhanh151098 <cao.minhanh151098@gmail.com>
2023-04-04 10:10:02 +0700

    fix test 1

--- Cau1/Cau1.js
+++ Cau1/Cau1.js
@@ -12,22 +12,37 @@ function quadratic() {
     let b = +document.getElementById("inputNum2").value;
     let c = +document.getElementById("inputNum3").value;
 
-    let delta = Math.pow(b, 2) - (4 * a * c);
+    if (a != 0) {
+        let delta = Math.pow(b, 2) - (4 * a * c);
 
-    if (delta > 0) {
-        result_1 = (-b + Math.sqrt(delta)) / (2 * a);
-        result_2 = (-b - Math.sqrt(delta)) / (2 * a);
-        alert(`Phương trình có 2 nghiệm:`);
-        alert(`x1 = ${result_1}`);
-        alert(`x2 = ${result_2}`);
+        if (delta > 0) {
+            result_1 = (-b + Math.sqrt(delta)) / (2 * a);
+            result_2 = (-b - Math.sqrt(delta)) / (2 * a);
+            alert(`Phương trình có 2 nghiệm:`);
+            alert(`x1 = ${result_1}`);
+            alert(`x2 = ${result_2}`);
+        }
+        else
+            if (delta == 0) {
+                result_1 = -b / 2 * a;
+                alert(`Phương trình nghiệm kép:`);
+                alert(`x = ${result_1}`);
+            }
+            else {
+                alert(`Phương trình vô ngiệm nghiệm:`);
+            }
     }
-    else
-        if (delta == 0) {
-            result_1 = -b / 2 * a;
-            alert(`Phương trình nghiệm kép:`);
-            alert(`x = ${result_1}`);
+    else {
+        if (b != 0) {
+            let result = -c / b;
+            alert(`Phương trình có 1 ngiệm: x = ${result}`);
         }
-        else {
-            alert(`Phương trình vô ngiệm nghiệm:`);
-        }
+        else
+            if (c != 0) {
+                alert(`Phương trình vô ngiệm`);
+            }
+            else {
+                alert(`Phương trình vô số ngiệm`);
+            }
+    }
 }
